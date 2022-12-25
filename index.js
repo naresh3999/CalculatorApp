ac7d0620d4a6c50b62fe98a9813b3a342e10aea4
naresh3999 <108687036+naresh3999@users.noreply.github.com>
2022-12-25 07:59:28 +0530

    Files added

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './SRC/API/SCREENS/FetchMosh';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
